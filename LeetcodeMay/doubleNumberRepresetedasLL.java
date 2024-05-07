ae3ec440f5593c923dd1318377f2ac6ad9e8db2f
Supratim Ghosh <109270340+sgindeed@users.noreply.github.com>
2024-05-07 16:07:56 +0530

    Create doubleNumberRepresetedasLL.java

---
+++ LeetcodeMay/doubleNumberRepresetedasLL.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode doubleIt(ListNode head) {
+
+        if (head.val > 4) {
+            head = new ListNode(0, head);
+        }
+
+        ListNode temp = head;
+        while (temp != null) {
+            temp.val = (temp.val * 2) % 10;
+            if (temp.next != null && temp.next.val > 4) {
+                temp.val++;
+            }
+
+            temp = temp.next;
+        }
+        return head;
+    }
+}
