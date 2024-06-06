b6df05b4ff07b889970e4ff8e449bc93d8e45c3b
Supratim Ghosh <109270340+sgindeed@users.noreply.github.com>
2024-06-06 12:46:43 +0530

    Update maxSumInConfiguration.java

--- maxSumInConfiguration.java
+++ maxSumInConfiguration.java
@@ -1,24 +1,24 @@
+
 class Solution {
     long max_sum(int a[], int n) {
-        long totalSum = 0;
-        long currentSum = 0;
-
-        // Calculate total sum of elements and initial sum
+        // Your code here
+          long S0 = 0;
+        long sum_a = 0;
+        
         for (int i = 0; i < n; i++) {
-            totalSum += a[i];
-            currentSum += i * a[i];
+            S0 += (long)i * a[i];
+            sum_a += (long)a[i];
         }
-
-        long maxSum = currentSum;
-
-        // Calculate the new sums for each rotation
-        for (int k = 1; k < n; k++) {
-            currentSum = currentSum + totalSum - n * a[n - k];
-            if (currentSum > maxSum) {
-                maxSum = currentSum;
-            }
+        
+        long maxSum = S0;
+        long currentSum = S0;
+    
+        for (int i = 1; i < n; i++) {
+            long val = currentSum - sum_a +((long) n * a[i-1]);
+            currentSum=val;
+            maxSum=Math.max(maxSum,val);
         }
-
+        
         return maxSum;
     }
 }
