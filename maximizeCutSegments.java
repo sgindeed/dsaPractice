a828e686a08c2d40e2efe8bc35a5cb5e787e6b97
Supratim Ghosh <109270340+sgindeed@users.noreply.github.com>
2024-08-16 09:12:55 +0530

    Create maximizeCutSegments.java

---
+++ maximizeCutSegments.java
@@ -0,0 +1,26 @@
+class Solution
+{
+    public int maximizeCuts(int n, int x, int y, int z)
+    {
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (i >= x && dp[i - x] != -1) {
+                dp[i] = Math.max(dp[i], dp[i - x] + 1);
+            }
+            if (i >= y && dp[i - y] != -1) {
+                dp[i] = Math.max(dp[i], dp[i - y] + 1);
+            }
+            if (i >= z && dp[i - z] != -1) {
+                dp[i] = Math.max(dp[i], dp[i - z] + 1);
+            }
+
+            if (dp[i] == 0) {
+                dp[i] = -1;
+            }
+        }
+
+        return dp[n] == -1 ? 0 : dp[n];
+    }
+}
