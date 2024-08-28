8a48ee0cfd771c1b9de04178fbda54ed35ed9daf
Supratim Ghosh <109270340+sgindeed@users.noreply.github.com>
2024-08-28 21:04:29 +0530

    Create RotateArray.java

---
+++ TCS NQT/RotateArray.java
@@ -0,0 +1,21 @@
+class Solution{
+  static void reverse(int arr[], int start, int end){
+    while(start < end){
+      int temp = arr[start];
+      arr[start] = arr[end];
+      arr[end] = temp;
+      start++;
+      end--;
+    }
+  }
+  
+  static void rotateArray(int arr[], int d, int n){
+    if(d == 0)
+        return;
+    d = d % n;
+
+    reverse(arr, 0, d-1);
+    reverse(arr, d, n-1);
+    reverse(arr, 0, n-1);
+  }
+}
