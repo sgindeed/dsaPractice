436180c0fb37cd9ea092c909ec3a6b65476197fa
Supratim Ghosh <109270340+sgindeed@users.noreply.github.com>
2024-10-29 20:50:29 +0530

    Create fibonacci.java

---
+++ dp/fibonacci.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class HelloWorld {
+    static int dp[];
+    
+    public static int fib(int a){
+        if(a <= 1)
+            return a;
+        if(dp[a] != -1)
+            return dp[a];
+        return fib(a-1) + fib(a-2);
+    }
+    
+    public static void main(String[] args) {
+        Scanner se = new Scanner(System.in);
+        int n = se.nextInt();
+        dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        System.out.println(fib(n));
+    }
+}
