0788d04765af45849bf3306018bdfda311f23929
Supratim Ghosh <109270340+sgindeed@users.noreply.github.com>
2024-11-27 06:40:25 +0530

    Create smallestMissingPositive.java

---
+++ smallestMissingPositive.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int missingNumber(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n; i++) {
+            while (arr[i] > 0 && arr[i] <= n && arr[arr[i] - 1] != arr[i]) {
+                int temp = arr[arr[i] - 1];
+                arr[arr[i] - 1] = arr[i];
+                arr[i] = temp;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        return n + 1;
+    }
+}
