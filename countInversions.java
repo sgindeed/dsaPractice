b1f5516b3a54fbebd123661807d5d378e1bb517d
Supratim <109270340+sgindeed@users.noreply.github.com>
2024-12-07 20:15:36 +0530

    Create countInversions.java

---
+++ countInversions.java
@@ -0,0 +1,53 @@
+class Solution {
+    static int inversionCount(int arr[]) {
+        return mergeSortAndCount(arr, 0, arr.length - 1);
+    }
+
+    static int mergeSortAndCount(int arr[], int left, int right) {
+        int count = 0;
+        if (left < right) {
+            int mid = (left + right) / 2;
+            count += mergeSortAndCount(arr, left, mid);
+            count += mergeSortAndCount(arr, mid + 1, right);
+            count += mergeAndCount(arr, left, mid, right);
+        }
+        return count;
+    }
+
+    static int mergeAndCount(int arr[], int left, int mid, int right) {
+        int count = 0;
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+        int[] leftArr = new int[n1];
+        int[] rightArr = new int[n2];
+        System.arraycopy(arr, left, leftArr, 0, n1);
+        System.arraycopy(arr, mid + 1, rightArr, 0, n2);
+
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k] = leftArr[i];
+                i++;
+            } else {
+                arr[k] = rightArr[j];
+                count += (n1 - i);
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = leftArr[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = rightArr[j];
+            j++;
+            k++;
+        }
+
+        return count;
+    }
+}
