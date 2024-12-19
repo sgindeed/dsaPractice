aaef5e2da10a76d9ab746afe183dcf42951645a8
Supratim <109270340+sgindeed@users.noreply.github.com>
2024-12-19 11:59:32 +0530

    Update

---
+++ kthPositiveMissing.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int kthMissing(int[] arr, int k) {
+        int left = 0, right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int missing = arr[mid] - (mid + 1);
+
+            if (missing < k) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return left + k;
+    }
+}
