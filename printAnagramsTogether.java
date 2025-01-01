44b5602656f4ca88ce417886b13ccca81e2ac2d2
Supratim <109270340+sgindeed@users.noreply.github.com>
2025-01-01 09:42:46 +0530

    Create printAnagramsTogether.java

---
+++ printAnagramsTogether.java
@@ -0,0 +1,16 @@
+ public ArrayList<ArrayList<String>> anagrams(String[] arr) {
+        // code here
+        TreeMap<String, ArrayList<String>> mp = new TreeMap<>();
+        int n = arr.length;
+        
+        for (String s : arr) {
+            char[] ch = s.toCharArray();
+            Arrays.sort(ch);
+            String str = new String(ch);
+            
+            mp.putIfAbsent(str, new ArrayList<>());
+            mp.get(str).add(s);
+        }
+        
+        return new ArrayList<>(mp.values());
+    }
